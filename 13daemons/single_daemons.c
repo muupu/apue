6e0d198d89686d3fe9c7719a28adfb20d686bded
muupu <once645@gmail.com>
2015-10-20 23:24:57 +0800

    comments

--- 13daemons/single_daemons.c
+++ 13daemons/single_daemons.c
@@ -23,16 +23,16 @@ already_running(void)
 		syslog(LOG_ERR, "can't open %s: %s", LOCKFILE, strerror(errno));
 		exit(1);
 	}
-	if (lockfile(fd) < 0) {
+	if (lockfile(fd) < 0) { // 如果该文件已经加锁，lockfile会失败
 		if (errno == EACCES || errno == EAGAIN) {
 			close(fd);
-			return(1);
+			return(1); // 返回1，表示该守护进程已经在运行
 		}
 		syslog(LOG_ERR, "can't lock %s: %s", LOCKFILE, strerror(errno));
 		exit(1);
 	}
-	ftruncate(fd, 0);
-	sprintf(buf, "%ld", (long)getpid());
+	ftruncate(fd, 0); // 需将文件长度截断为0。或者进程ID可能无法完全覆盖之前的进程ID字符串
+	sprintf(buf, "%ld", (long)getpid()); // 将进程ID写进文件中，方便标识进程
 	write(fd, buf, strlen(buf)+1);
 	return(0);
 }
