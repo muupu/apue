3ae65e5e96930ae6fbee8a7c2ee0f6e497e2837e
muupu <once645@gmail.com>
2015-10-24 22:57:56 +0800

    comments

--- 14advanceIO/rot13a.c
+++ 14advanceIO/rot13a.c
@@ -29,15 +29,15 @@ main(int argc, char* argv[])
 
 	if (argc != 3)
 		err_quit("usage: rot13 infile outfile");
-	if ((ifd = open(argv[1], O_RDONLY)) < 0)
-		err_sys("can't open %s", argv[1]);
+	if ((ifd = open(argv[1], O_RDONLY)) < 0) // 打开输入文件
+		err_sys("can't open %s", argv[1]);   // 打开输出文件
 	if ((ofd = open(argv[2], O_RDWR|O_CREAT|O_TRUNC, FILE_MODE)) < 0)
 		err_sys("can't create %s", argv[2]);
 
-	while ((n = read(ifd, buf, BSZ)) > 0) {
+	while ((n = read(ifd, buf, BSZ)) > 0) { // 从输入文件中取出一个块
 		for (i = 0; i < n; i++)
-			buf[i] = translate(buf[i]);
-		if ((nw = write(ofd, buf, n)) != n) {
+			buf[i] = translate(buf[i]); // 翻译字符
+		if ((nw = write(ofd, buf, n)) != n) { // 写入输出文件中
 			if (nw < 0)
 				err_sys("write failed");
 			else
