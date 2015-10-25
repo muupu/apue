db372c404328c37b83b7e06464a0435b9c8fb10f
muupu <once645@gmail.com>
2015-10-25 23:15:56 +0800

    comments

--- 15IPC/pip1.c
+++ 15IPC/pip1.c
@@ -1,5 +1,8 @@
 #include "apue.h"
 
+/*
+创建一个从父进程到子进程的管道
+*/
 int
 main(void)
 {
@@ -8,16 +11,16 @@ main(void)
 	pid_t	pid;
 	char	line[MAXLINE];
 
-	if (pipe(fd) < 0)
+	if (pipe(fd) < 0)  /* 创建管道 */
 		err_sys("pipe error");
 	if ((pid = fork()) < 0) {
 		err_sys("fork error");
-	} else if (pid > 0) {		/* parent */
+	} else if (pid > 0) {		/* parent 关闭父进程的读端 */
 		close(fd[0]);
-		write(fd[1], "hello world\n", 12);
-	} else {					/* child */
+		write(fd[1], "hello world\n", 12); /* 父进程写数据到管道 */
+	} else {					/* child 关闭子进程的写端*/
 		close(fd[1]);
-		n = read(fd[0], line, MAXLINE);
+		n = read(fd[0], line, MAXLINE); /* 子进程从管道读数据 */
 		write(STDOUT_FILENO, line, n);
 	}
 	exit(0);
