2e93f03b584e0534e5d439f5d43ec9b4ad76f5bf
muupu <once645@gmail.com>
2015-11-03 22:53:59 +0800

    more comments: parent process

--- 17UnixIPC/open/open.c
+++ 17UnixIPC/open/open.c
@@ -43,11 +43,11 @@ csopen(char *name, int oflag)
 	iov[2].iov_base = buf;
 	iov[2].iov_len  = strlen(buf) + 1;	/* +1 for null at end of buf */
 	len = iov[0].iov_len + iov[1].iov_len + iov[2].iov_len;
-	if (writev(fd[0], &iov[0], 3) != len) {
+	if (writev(fd[0], &iov[0], 3) != len) { // 父进程将包含路径名和打开模式的请求发送给服务器进程
 		err_ret("writev error");
 		return(-1);
 	}
 
-	/* read descriptor, returned errors handled by write() */
-	return(recv_fd(fd[0], write));
+	/* read descriptor, returned errors handled by write() 父进程调用recv_fd返回描述符或出错消息*/
+	return(recv_fd(fd[0], write)); 
 }
